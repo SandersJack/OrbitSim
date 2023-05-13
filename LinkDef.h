174acf98de6f77977c82de2f04f4698e8e1dfe18
JackLaptop <sandersjack141@gmail.com>
2023-05-14 01:19:21 +0200

    Added LinkDef

---
+++ LinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __MAKECINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// Turn on creation of dictionaries for nested classes
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class Vector3D+;
+#endif
