378415b47f3e7114761bf415ea6422d8b9e511da
JackLaptop <sandersjack141@gmail.com>
2023-05-14 19:46:03 +0200

    Cleaned up build of cmake

--- Planets/Earth/CMakeFiles/Earth-static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libEarth-static.a"
-  "libEarth-static.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/Earth-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Planets/Earth/CMakeFiles/EarthObjLib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for EarthObjLib.
-# This may be replaced when dependencies are built.
