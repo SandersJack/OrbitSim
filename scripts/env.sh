c1668c983a5462f287590e9374ff390ede361622
JackLaptop <sandersjack141@gmail.com>
2023-05-14 19:46:21 +0200

    updated alias to make building easier

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,6 +2,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$(dirname "$(dirname "${DIR}")")
 
 export ORBITSOURCE="${DIR}/Orbits"
-alias ORBITS="${ORBITSOURCE}/bin/OrbitSim"
+alias ORBITS="${ORBITSOURCE}/build/OrbitSim"
+
+alias make="cmake -Bbuild/ -S.; cd build; make; cd .."
 
 source /home/jack/Code/root/bin/thisroot.sh
