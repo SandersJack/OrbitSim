23048a199e2811c4acbab5cbb5e2904815880738
JackLaptop <sandersjack141@gmail.com>
2023-05-17 22:27:35 +0200

    Updated checking code for new setup

--- Analysis/Checking.py
+++ Analysis/Checking.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 file = uproot.open("/home/jack/Code/DevTraining/Apps/Orbits/Oribts.root")
 
-fx_earth = file["Data"]["Earth/fx"].array()
-fy_earth = file["Data"]["Earth/fy"].array()
+fx_earth = file["Data"]["Earth/Planets/fPosition.fx"].array()
+fy_earth = file["Data"]["Earth/Planets/fPosition.fy"].array()
 
-fx_mars = file["Data"]["Mars/fx"].array()
-fy_mars = file["Data"]["Mars/fy"].array()
+fx_mars = file["Data"]["Mars/Planets/fPosition.fx"].array()
+fy_mars = file["Data"]["Mars/Planets/fPosition.fy"].array()
 
 fig = plt.figure(figsize=(10,10))
 plt.scatter(fx_earth,fy_earth)
