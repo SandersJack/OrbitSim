6c1b932c4d4aaf0c67f0bec749db025b71b0b064
JackLaptop <sandersjack141@gmail.com>
2023-05-28 09:13:27 +0200

    Added command line params

--- Orbits.cc
+++ Orbits.cc
@@ -5,7 +5,7 @@
 #include <string>
 
 
-int main(){
+int main(int argc, char* argv[]){
     // Default Parameters
     std::string infile = "config/PlanetsList.dat";
     std::string Mooninfile = "config/MoonsList.dat";
@@ -13,6 +13,31 @@ int main(){
     double timeStep = 86400; //seconds
     double stopTime = 1000; //days
 
+    // Parse inputs
+    for (int i = 1; i < argc; i++)
+    {  
+        if (i + 1 != argc)
+        {
+            if (strcmp(argv[i], "-Ts") == 0) // TimeStep
+            {                 
+                timeStep = std::stod(argv[i + 1]);
+                i++;
+            } 
+            if (strcmp(argv[i], "-St") == 0) // StopTime
+            {                 
+                stopTime = std::stod(argv[i + 1]);
+                i++;
+            }
+        }
+        if (strcmp(argv[i], "-h") == 0) // Help
+            {                 
+                std::cout << "Displaying Help Message:" << std::endl;
+                std::cout << "-Ts <double> : Time step in seconds" << std::endl;
+                std::cout << "-----------------------------------" << std::endl;
+                std::cout << "-St <double> : Total time in days" << std::endl;
+                exit(0);
+        }
+    }
     std::cout << "##########################################################" << std::endl;
     std::cout << "##    Running Orbit Simulation using Parameters: " << "       ##" << std::endl;
     std::cout << "##    Time Step = " << timeStep/(24*60*60) << " days. Total Time = " << stopTime << " days."  << "       ##" << std::endl;
