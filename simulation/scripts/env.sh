12bd36ed548e012c835f078fcd0db4978e82becb
SandersJack <sandersjack141@gmail.com>
2024-03-05 16:22:16 +0000

    moved commands from env into a make file

--- simulation/scripts/env.sh
+++ simulation/scripts/env.sh
@@ -4,10 +4,6 @@ DIR=$(dirname "$(dirname "${DIR}")")
 export ORBITSOURCE="${DIR}/simulation"
 alias ORBITS="${ORBITSOURCE}/build/OrbitSim"
 
-alias make_clean="rm build/* -r"
-
-alias make="cmake -Bbuild/ -S.; cd build; make; cd .."
-
 source ~/root/bin/thisroot.sh
 
 source ../.venv/bin/activate
